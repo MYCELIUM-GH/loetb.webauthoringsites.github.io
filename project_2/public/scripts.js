f98812ade378b94d3a47a0a082b05bfded61879d
MYCELIUM-GH <felelem.s@gmail.com>
2025-04-04 14:30:45 +0100

    some sht

--- project_2/public/scripts.js
+++ project_2/public/scripts.js
@@ -1,3 +1,14 @@
+// ==== VARIABLES
+var timer;
+// ==== LOADER
+function loader() {
+	timer = setTimeout(showPage, 3000);
+};
+function showPage() {
+	document.getElementById("loader").style.display = "none";
+	document.getElementById("page-full").style.display = "block";
+};
+// ==== NAVBAR
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     $("#mainListDiv").toggleClass("show_list");
