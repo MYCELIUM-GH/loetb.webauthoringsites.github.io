f821374bdc59183261766687d496c7811697a2f0
MYCELIUM-GH <felelem.s@gmail.com>
2025-04-04 14:41:21 +0100

    loader added + some fixes

--- project_2/public/scripts.js
+++ project_2/public/scripts.js
@@ -1,5 +1,15 @@
+// ==== NAVBAR
 $('.navTrigger').click(function () {
     $(this).toggleClass('active');
     $("#mainListDiv").toggleClass("show_list");
     $("#mainListDiv").fadeIn();
 });
+// ==== LOADER
+var timer;
+function loader() {
+	timer = setTimeout(showPage, 3000);
+};
+function showPage() {
+	document.getElementById("loader").style.display = "none";
+	document.getElementById("page-full").style.display = "block";
+};
